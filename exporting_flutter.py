46f7512dc6ae3d6e960b356cc2415f93bf5194b7
Petr Doležal <sirelkir@gmail.com>
2022-06-14 15:10:38 +0100

    end of hackathon state - generalized for variables

--- exporting_flutter.py
+++ exporting_flutter.py
@@ -3,12 +3,19 @@ import xarray as xr
 import cartopy.crs as ccrs
 
 from pathlib import Path
-
-from flutter import export_flutter
+import flutter
 
 dir_data = Path("data/reduced")
 dir_fig = Path("data/fig")
-dir_vid = dir_fig/ "vid/refactored"
+dir_vid = dir_fig/ "vid"
+
+da = xr.load_dataset(dir_data/"hundred_members_2t_pf.nc")
+da = da.assign_coords({"longitude": [ i if i <= 180 else i-360 for i in da.longitude.values]})
+# reduce spatial range
+da = da.sel(longitude=slice(-20,20),latitude=slice(65,35))
+
+
+
 
 # Es=[50]
 Es=[50,100] 
@@ -21,21 +28,17 @@ n_repetitions = 2
 shuffle = True
 
 temp_threshold = 268
-params_temp = dict(vmin=250, vmax=290)
 # temp_threshold = 273.15 
+params_temp = dict(vmin=250, vmax=290)
 
-
-da = xr.load_dataset(dir_data/"hundred_members_2t_pf.nc")
-da = da.assign_coords({"longitude": [ i if i <= 180 else i-360 for i in da.longitude.values]})
-# reduce spatial range
-da = da.sel(longitude=slice(-20,20),latitude=slice(65,35))
 
 for E in Es:
-    export_flutter(da,Ts,E,
-            filename=dir_vid/f"ensembles_time_freezing_T{timesteps}_{E}.mp4",
+    flutter.export_flutter(da,Ts,E,
+            filename=dir_vid/f"ensembles_time_subfreezing_T{timesteps}_{E}.mp4",
             projection= ccrs.PlateCarree(),
             transform = ccrs.PlateCarree(),
             plot_params=params_temp,
             threshold=temp_threshold,
-            n_repetitions=n_repetitions, shuffle=shuffle
+            n_repetitions=n_repetitions, shuffle=shuffle,
+            title=f"Temperatures <-5⁰C N_ens:{E:3}"
             )
--- flutter.py
+++ flutter.py
@@ -19,81 +19,85 @@ def datef(X):
 
 
     
-def animate_time(da, Ts, E, ax, threshold, plot_params,
-                n_repetitions=2,shuffle=False):
-    #returns list of artists
-    artists = []
-    
-    for T in Ts:
-        dt = da.isel(time=T)
-        artists.extend(animate_timestep(dt,E, ax, 
-                                        threshold=threshold, plot_params=plot_params,
-                                        n_repetitions=n_repetitions,shuffle=shuffle))
-    return artists
-    
-def animate_timestep(dt, E,ax,
+def animate_timestep(dt,var, E,ax,
                      threshold=None,
                      plot_params=dict(),
-                     n_repetitions=1, shuffle=False
-                     ):
+                     n_repetitions=1, shuffle=False,
+                     title=None):
     #returns list of artists
     
     import random
     artists = []
     artists_T = []
     for E in range(1,101,100//E):
-        ds = dt.sel(number=E).t2m
+        ds = dt.sel(number=E)[var]
         
         # threshold on freezing 
         if threshold is not None:
             ds = ds.where(ds<threshold) 
         
-        im = ds.plot(animated=True,add_colorbar=False,**plot_params)
+        im = ds.plot(animated=True,add_colorbar=False,ax=ax,**plot_params)
         # if E==1:
         #    ds.plot(**params, animated=True)
+        if title is not None:
+            ax.set_title(title) 
         artists_T.append([im])
-        ax.set_title(f"N_ens:{E:3}  time: {datef(dt)}") 
+        
     if shuffle:
         for _ in range(n_repetitions):
             random.shuffle(artists_T)
             artists.extend(artists_T)
-        else:
-            artists.extend(artists_T*n_repetitions)
+    else:
+        artists.extend(artists_T*n_repetitions)
             
     return artists
     
+def animate_time(da, var, Ts, E, ax, threshold, plot_params,
+                n_repetitions=2,shuffle=False,
+                title=None):
+    #returns list of artists
+    artists = []
     
-## Animation
-# evolving timesteps 
-# threshold temperature - freezing
+    for T in Ts:
+        dt = da.isel(time=T)
+        artists.extend(animate_timestep(dt,var, E, ax=ax, 
+                                        threshold=threshold, plot_params=plot_params,
+                                        n_repetitions=n_repetitions,shuffle=shuffle,
+                                        title=title))
+        if title is not None:
+            ax.set_title(title)     
+    return artists
+    
+    
 
-
-
-
-
-def export_flutter(da,Ts,E,filename,
+def export_flutter(da,var,Ts,E,filename,
             projection, transform,
             plot_params=dict(),
             threshold=None,
-            n_repetitions=2,shuffle=False
+            n_repetitions=2,shuffle=False,
+            title=None
             ):
     # Generate flutter video given dataset, timesteps and ensembles
     
     plt.close()
     fig, ax = plt.subplots(subplot_kw=dict(projection=projection))
+    fig.tight_layout(pad=0)
     ax.coastlines(resolution='50m')
-    
+    if title is not None:
+        ax.set_title(title) 
+        
     plot_params = plot_params.copy()
     plot_params['transform']=transform
     
-    artists = animate_time(da,Ts,E, ax=ax,
+    artists = animate_time(da,var,Ts,E, ax=ax,
                            threshold=threshold,plot_params=plot_params,
-                           n_repetitions=n_repetitions,shuffle=shuffle)
+                           n_repetitions=n_repetitions,shuffle=shuffle,
+                           title=title)
 
 
     ani = ArtistAnimation(fig, artists, interval=1700//E, blit=True,
                                     repeat_delay=1000)
-
+   
     ani.save(filename)
     
     # generates flutter video and saves it
